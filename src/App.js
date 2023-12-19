80352b935d47f26aedc089529b9aecc0866d850a
delvissoto <delvissoto99@icloud.com>
2023-12-18 21:50:14 -0600

    css navbar responsive line

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li><a href=''>Home</a></li>
+          <li><a href=''>About us</a></li>
+          <li><a href=''>Portfolio</a></li>
+          <li><a href=''>Stats</a></li>
+          <li><a href=''>Who is that?</a></li>
+         
+          
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
